9a69dc349ee00fce5b73ab062a08a70d1a656ceb
Zkanics F. P. S. <ryosuke_ishizuka@shokubai.co.jp>
2025-12-11 14:25:15 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import io
-from cielab.main import mkslide_gui
+from cielab.main import cielab_gui
 
 st.image("zkanics_cute_logo.png", caption="Supported by Zkanics F. P. S. since 2024", width=250)
 st.markdown("---")
